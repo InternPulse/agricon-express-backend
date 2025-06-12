3e6461a897200f61f177dd73bcd8df8cc22469f7
Chinweike99 <innocentakwolu1@gmail.com>
2025-06-12 16:53:30 +0100

    chore: setup cd to render

--- dist/index.js
+++ dist/index.js
@@ -1,5 +1,8 @@
 import express from 'express';
 const app = express();
+app.get('/', (req, res) => {
+    res.send('AGRICON');
+});
 app.listen(3000, () => {
     console.log("Listening on port 3000");
 });
