7cb2728a36df9d0672f06b7aa44d77af411082d8
Chinweike99 <innocentakwolu1@gmail.com>
2025-06-14 11:55:17 +0100

    fix: remove causing duplication

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,7 @@
-import dbtest from "./test_database_connection/app";
 import app from './app';
-const port = process.env.PORT || 3000;
+import { config } from './config/config.env';
+const port = config.PORT;
 
-// const app = express();
-
-// testConnection();
-
-dbtest.get("/", (req, res) => {
-  try {
-    res.send("AGRICON – Internpulse Project, Cohort 8 (2025)");
-  } catch (error) {
-    console.log(error)
-    res.status(400).json({message: "Failed", error: error})
-  }
-});
-
-// dbtest.listen(port, () => {
-//   console.log(`Listening on port localhost:${port}`);
-// });
 
 app.listen(port, () => {
   console.log(`Booking Listening on port localhost:${port}`);
