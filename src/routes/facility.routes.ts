cb279d87fc127a7d5a64b2cb63a8d3eacbd7acd4
Adeyemi Rachael <adeyemitoluadedoyin@gmail.com>
2025-06-16 17:37:05 +0100

    feat: facility post route

--- src/routes/facility.routes.ts
+++ src/routes/facility.routes.ts
@@ -1,9 +1,10 @@
 import express from 'express';
-import { getFacility, updateFacility } from '../controllers/facility.controller';
+import { addFacility, getFacility, updateFacility } from '../controllers/facility.controller';
 import { isFacilityOwner } from '../middlewares/authorization.middlewares';
 
 const router = express.Router();
 
+router.post('/facility', addFacility);
 router.get('/:facilityId', isFacilityOwner, getFacility)
 router.put('/:facilityId', isFacilityOwner, updateFacility)
 
