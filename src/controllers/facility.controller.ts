248069acd9173a546c20abf6792a11db9e3d028d
Adeyemi Rachael <adeyemitoluadedoyin@gmail.com>
2025-06-17 10:55:40 +0100

    feat: create facility controller

--- src/controllers/facility.controller.ts
+++ src/controllers/facility.controller.ts
@@ -1,6 +1,15 @@
 import { Request, Response } from "express";
-import { get, update } from "../services/db/facility.service";
+import { create, get, update } from "../services/db/facility.service";
 import { StatusCodes } from "http-status-codes";
+
+export const addFacility = async (req: Request, res: Response) => {
+  const facility = await create(req.body);
+  res.status(StatusCodes.CREATED).json({
+    success: true,
+    message: "Facility created successfully",
+    data: facility
+  });
+}
 
 export const getFacility = async (req: Request, res: Response) => {
   const facilityId = req.params.facilityId;
