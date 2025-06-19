8727cd9fa97abdf3cd698d8de642093222a4d342
Chinweike99 <innocentakwolu1@gmail.com>
2025-06-19 16:49:43 +0100

    fix

--- src/routes/booking.routes.ts
+++ src/routes/booking.routes.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import { deleteBooking, fetchBooking, listFarmerBookings } from '../controllers/booking.controller';
+import {deleteBookingHandler, fetchBooking, listFarmerBookings } from '../controllers/booking.controller';
 import { validateBookingId } from '../middlewares/bookingValidation';
 import { authorizeRole, checkBookingOwnership } from '../middlewares/authorization.middlewares';
 import { UserRole } from '../types/types';
@@ -9,10 +9,11 @@ import { updateBookingHandler } from '../controllers/updatebooking.controller';
 
 const router = express.Router();
 
-router.delete('/:bookingId', validateBookingId, checkBookingOwnership, deleteBooking);
+router.delete('/:bookingId', validateBookingId, checkBookingOwnership, deleteBookingHandler);
 router.post('/create-booking', verifyAuth, authorizeRole([UserRole.FARMER, UserRole.OPERATOR]), createBookingHandler)
 router.patch('/:bookingId', verifyAuth, validateBookingId,  updateBookingHandler);
 router.get('/:bookingId', verifyAuth, validateBookingId, fetchBooking)
+router.delete('/:bookingId', verifyAuth, validateBookingId, deleteBookingHandler)
 
 router.get('/', listFarmerBookings);
 
