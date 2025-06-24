6b33f1dac9f79510701c05b5e2889bfaa6b875cd
Adeyemi Rachael <adeyemitoluadedoyin@gmail.com>
2025-06-24 07:13:55 +0100

    feat: added function to route

--- src/routes/facility.routes.ts
+++ src/routes/facility.routes.ts
@@ -2,7 +2,7 @@ import express from 'express';
 import { facilityValidator } from '../utils/validateFacility';
 import { verifyAuth } from '../middlewares/authenticate.middleware';
 import { isFacilityOwner, isOperator } from '../middlewares/authorization.middlewares';
-import {  addFacility,getFacility, updateFacility, getAllFacility, removeFacility } from '../controllers/facility.controller';
+import {  addFacility,getFacility, updateFacility, getAllFacility, removeFacility, updateFacilityCapacity } from '../controllers/facility.controller';
 
 const router = express.Router();
 
@@ -11,5 +11,6 @@ router.get('/', verifyAuth, getAllFacility);
 router.get('/:facilityId', verifyAuth,  getFacility);
 router.put('/:facilityId', verifyAuth, isFacilityOwner, updateFacility);
 router.delete('/:facilityId', verifyAuth, isFacilityOwner, removeFacility);
+router.get('/capacity', verifyAuth, updateFacilityCapacity);
 
 export default router;
