c5ec708c1da6c44b0679c70f3219d152d818b02f
chinasa056 <chinasaacha05@gmail.com>
2025-06-25 14:13:43 +0100

    chore: added booking date validator to ensure booking date cannot be changed after payment

--- src/routes/facility.routes.ts
+++ src/routes/facility.routes.ts
@@ -2,7 +2,7 @@ import express from 'express';
 import { facilityValidator } from '../utils/validateFacility';
 import { verifyAuth } from '../middlewares/authenticate.middleware';
 import { isFacilityOwner, isOperator } from '../middlewares/authorization.middlewares';
-import {  addFacility,getFacility, updateFacility, getAllFacility, removeFacility } from '../controllers/facility.controller';
+import {  addFacility,getFacility, updateFacility, getAllFacility, removeFacility, getAllFacilitiesController, getFacilitiesByOperatorController } from '../controllers/facility.controller';
 
 const router = express.Router();
 
@@ -11,5 +11,7 @@ router.get('/', verifyAuth, getAllFacility);
 router.get('/:facilityId', verifyAuth,  getFacility);
 router.put('/:facilityId', verifyAuth, isFacilityOwner, updateFacility);
 router.delete('/:facilityId', verifyAuth, isFacilityOwner, removeFacility);
+router.get('/get', getAllFacilitiesController);
+router.get('/:operatorId', getFacilitiesByOperatorController);
 
 export default router;
