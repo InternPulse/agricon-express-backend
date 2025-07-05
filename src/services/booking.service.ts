4c890925c39e1142f7e918c874346f3f8541fbe5
Chinweike99 <innocentakwolu1@gmail.com>
2025-07-05 08:16:23 +0100

    test

--- src/services/booking.service.ts
+++ src/services/booking.service.ts
@@ -109,18 +109,30 @@ export const createBooking = async (data: CreateBookingParams) => {
         data.endDate
       );
 
+    // return prisma.booking.create({
+    //   data: {
+    //     facility: { connect: { id: BigInt(data.facilityId) } },
+    //     farmer: { connect: { id: BigInt(data.farmerId) } },
+    //     startDate: data.startDate,
+    //     endDate: data.endDate,
+    //     amount,
+    //     paid: false,
+    //     active: true,
+    //   },
+    //   include: { facility: true, farmer: true },
+    // });
     return prisma.booking.create({
-      data: {
-        facility: { connect: { id: BigInt(data.facilityId) } },
-        farmer: { connect: { id: BigInt(data.farmerId) } },
-        startDate: data.startDate,
-        endDate: data.endDate,
-        amount,
-        paid: false,
-        active: true,
-      },
-      include: { facility: true, farmer: true },
-    });
+  data: {
+    facilityId: BigInt(data.facilityId),
+    farmerId: BigInt(data.farmerId),
+    startDate: data.startDate,
+    endDate: data.endDate,
+    amount,
+    paid: false,
+    active: true,
+  },
+  include: { facility: true, farmer: true },
+});
   } catch (error) {
     console.log("Failed to create booking", error);
     throw error;
